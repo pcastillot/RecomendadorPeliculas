846c1d422a470875d9805247356814e468dc04aa
Pedro <pcastillotorio@gmail.com>
2021-05-11 22:08:10 +0200

    Carga de datos en la tabla
    Se carga en la tabla los ratings realizados por el usuario seleccionado en el combobox (temporal, prueba de carga de datos en la tabla)

--- dbManager.py
+++ dbManager.py
@@ -3,11 +3,13 @@ import sqlite3 as sqlite
 sqlite_conexion = sqlite.connect('MovieLens.db')
 cursor = sqlite_conexion.cursor()
 
+
 def getUsuarios():
     query = 'SELECT DISTINCT userId from ratings'
     cursor.execute(query)
     userIds = cursor.fetchall()
     return userIds
+
 
 def getPeliculas():
     query = 'SELECT movieId, title from movies'
@@ -15,5 +17,11 @@ def getPeliculas():
     peliculas = cursor.fetchall()
     return peliculas
 
-def getPelicula(pelicula):
-    return 0
+
+def getRatingsUsuario(usuario):
+    query = '''SELECT ratings.movieId, movies.title, ratings.rating FROM ratings 
+                JOIN movies ON ratings.movieId = movies.movieId 
+                where userId=?'''
+    cursor.execute(query, (usuario,))
+    ratings = cursor.fetchall()
+    return ratings
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import dbManager
 
 class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     peliculas = dict
+
     def __init__(self, *args, **kwargs):
         #Inicializacion de la ventana y listeners
         QtWidgets.QMainWindow.__init__(self, *args, **kwargs)
@@ -13,13 +14,24 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.cargarPeliculas()
 
         #Signals
-        self.btnRecomendarTabla.clicked.connect(lambda: self.recomendarRanking(self.cbUsuariosRanking.currentText()))
+        self.btnRecomendarTabla.clicked.connect(lambda: self.mostrarRatings(self.cbUsuariosRanking.currentText()))
         self.btnRecomendarPelicula.clicked.connect(lambda: self.recomendarPelicula(self.cbUsuariosPelicula.currentText()
                                                                                    , self.cbPeliculas.currentText()))
 
     # Recomendar y cargar la tabla de ranking
     def recomendarRanking(self, usuario):
         return 0
+
+    # Funcion de prueba para probar la carga de datos en la tabla
+    def mostrarRatings(self, usuario):
+        ratings = dbManager.getRatingsUsuario(usuario)
+        self.tbRanking.setRowCount(len(ratings))
+        row = 0
+        for rating in ratings:
+            self.tbRanking.setItem(row, 0, QtWidgets.QTableWidgetItem(rating[0]))
+            self.tbRanking.setItem(row, 1, QtWidgets.QTableWidgetItem(rating[1]))
+            self.tbRanking.setItem(row, 2, QtWidgets.QTableWidgetItem(rating[2]))
+            row += 1
 
     # Predecir la puntuación que el usuario le daría a x película
     def recomendarPelicula(self, usuario, pelicula):
@@ -37,9 +49,6 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         for usuario in usuarios:
             self.cbUsuariosRanking.addItem(usuario[0])
             self.cbUsuariosPelicula.addItem(usuario[0])
-
-
-
 
 
 if __name__ == "__main__":
