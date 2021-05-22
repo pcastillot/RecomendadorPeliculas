4cc2ffc497fd92509b064d57c888923b40b243d3
zeldafan22 <riperhior@gmail.com>
2021-05-22 18:25:27 +0200

    Implementadas función ajustar matriz y media usuario
    Implementadas las funciones de media usuario, que calcula la media de valoraciones de un usuario, y la de ajustar matriz, que recibe la matriz de ratings y ajusta las valoraciones con la media de cada uno de los usuarios

--- main.py
+++ main.py
@@ -32,7 +32,18 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
             self.tbRanking.setItem(row, 1, QtWidgets.QTableWidgetItem(rating[1]))
             self.tbRanking.setItem(row, 2, QtWidgets.QTableWidgetItem(rating[2]))
             row += 1
-        self.predecir()
+
+        #Pruebas
+        '''
+        #self.predecir()
+        usuarios = [[1,2],[4,5],[1,5],[3,4],[5,5]]
+        #self.cosenoAjustado(usuarios)
+        #self.media_usuario(1)
+        ratings = dbManager.getRatings()
+        matriz_ajustada = self.ajustarDatos(ratings)
+        #print(matriz_ajustada[0])
+        '''
+
 
     # Predecir la puntuación que el usuario le daría a x película
     def recomendarPelicula(self, usuario, pelicula):
@@ -44,26 +55,66 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
             self.peliculas.update({pelicula[0]: pelicula[1]})
             self.cbPeliculas.addItem(pelicula[1])
 
-    def cosenoAjustado(self): #puede cambiar
-        #datos ejemplo (en su lugar importar los datos del archivo db)
-        usuarios = [[1,2,3],[4,5,3],[1,5,4],[3,4,4],[5,5,1]]
-
+    #Función que ajusta las valoraciones de todos los usuarios en función de su media para aplicar la fórmula del coseno
+    def ajustarDatos(self, List):
+        matriz = List
+        usuarios = dbManager.getUsuarios()
+        #Cargamos los usuarios y vamos uno a uno
+        for user in usuarios:
+            #Limpiamos el campo para poder usar el valor del ID de cada usuario
+            string_aux = str(user[0]).replace("(","").replace(")","").replace(",","").replace("'","")
+            #Se llama a la función de calcular media para obtener la media del usuario
+            media = float(self.media_usuario(string_aux))
+            i = 0
+            #Vamos recorriendo la matriz de valoraciones fila a fila
+            for fila in matriz:
+                #Comprobamos cada fila para ver si el usuario de esa fila y el usuario que estamos comprobando son
+                #el mismo.
+                if (int(str(fila[0]).replace("'",""))==(int(string_aux))):
+                    #En caso de ser el mismo, ajustamos el dato de valoración restando su valoración media
+                    aux = float(str(fila[3]).replace("'",""))
+                    aux2 = format((aux - media), ".3f")
+                    #Una vez calculado el nuevo valor lo insertamos de nuevo en la matriz
+                    fila_aux = (fila[0], fila[1], fila[2], aux2)
+                    fila = fila_aux
+                    matriz[i] = fila
+                i += 1 
+        return matriz   
+        
+    #Aplica la fórmula del coseno a la lista insertada
+    def formulaCoseno(self, List):
+        usuarios = List
         #variables para usar
-        numerador = 0
-        denominador_1 = 0
-        denominador_2 = 0
+        numerador = 0.0
+        denominador_1 = 0.0
+        denominador_2 = 0.0
 
         #realiza los sumatorios de la fórmula y los guarda en variables
         for i in usuarios:
-            numerador += (i[0]-i[2])*(i[1]-i[2])
-            denominador_1 += (i[0]-i[2])**2
-            denominador_2 += (i[1]-i[2])**2
+            numerador += (i[0])*(i[1])
+            denominador_1 += (i[0])**2
+            denominador_2 += (i[1])**2
 
         #aplica la fórmula del coseno ajustado utilizando los sumatorios previamente calculados.
         coseno_ajustado = numerador/((denominador_1**(1/2))*(denominador_2**(1/2)))
 
         #devuelve el coeficiente de similitud
         print(coseno_ajustado)
+
+
+    #Calcula el valor medio de todas las valoraciones de un usuario
+    def media_usuario(self, int):
+        user_id = int
+        media = 0.0
+
+        ratings = dbManager.getRatingsUsuario(user_id)
+        
+        for rating in ratings:
+            media+=float(rating[2])
+
+        media = media/len(ratings)
+        media = (format(media,".3f"))
+        return media
 
     def predecir(self):
         #Declaración de variables a usar
@@ -104,7 +155,7 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
                 if (pelicula in aux_vistas):
                     usuarios_similares.append(string_aux)
             print(usuarios_similares)
-            prediccion = "e"
+            prediccion = "Prediccion: e"
         else:
             prediccion = "Película ya vista, seleccione otra"
         print(prediccion)
